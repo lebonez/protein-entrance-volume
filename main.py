currently not working needs boundary calculation fixed.

main.py
@@ -11,6 +11,7 @@ from protein_entrance_volume.atoms import Atoms
 from protein_entrance_volume.grid import Grid
 from protein_entrance_volume import boundary
 from protein_entrance_volume import surface
+from protein_entrance_volume import visualization
 from protein_entrance_volume import utils
 
 
@@ -63,21 +64,21 @@ def main():
 
     # TODO: Don't append inside the class but only returns the new sphere then
     # append here. Also needs better documenting and commenting.
-    boundary_spheres = boundary.Spheres(mbr_coords, mbr_radii, mbr_distance, atoms.arc, extension=args.probe_radius)
+    boundary_points = boundary.Sphere(mbr_distance, atoms.arc, extension=args.probe_radius).coords
 
-    if not args.no_outer:
-        closest_coord = atoms.or_coords[utils.closest_node(atoms.orc, atoms.or_coords)]
-        distance = np.linalg.norm(closest_coord - atoms.orc)
-        boundary_spheres = boundary.HalfSpheres(atoms.or_coords, boundary_spheres.coords, boundary_spheres.radii, distance, atoms.orc, atoms.irc, extension=args.probe_radius)
-
-    if not args.no_inner:
-        closest_coord = atoms.ir_coords[utils.closest_node(atoms.irc, atoms.ir_coords)]
-        distance = np.linalg.norm(closest_coord - atoms.irc)
-        boundary_spheres = boundary.HalfSpheres(atoms.ir_coords, boundary_spheres.coords, boundary_spheres.radii, distance, atoms.irc, atoms.orc, extension=args.probe_radius)
+    # if not args.no_outer:
+    #     closest_coord = atoms.or_coords[utils.closest_node(atoms.orc, atoms.or_coords)]
+    #     distance = np.linalg.norm(closest_coord - atoms.orc)
+    #     boundary_points.extend(boundary.HalfSphere(atoms.or_coords, distance, atoms.orc, atoms.irc, extension=args.probe_radius).coords)
+    #
+    # if not args.no_inner:
+    #     closest_coord = atoms.ir_coords[utils.closest_node(atoms.irc, atoms.ir_coords)]
+    #     distance = np.linalg.norm(closest_coord - atoms.irc)
+    #     boundary_points.extend(boundary.HalfSphere(atoms.ir_coords, distance, atoms.irc, atoms.orc, extension=args.probe_radius).coords)
     # ENDTODO
 
     # Calculate the entrance volume features.
-    sas = surface.Surface(boundary_spheres.coords, boundary_spheres.radii, atoms.orc, num_points=1000)
+    sas = surface.Surface(mbr_coords, mbr_radii, centroid=atoms.orc, boundary_points=boundary_points, num_points=1000)
 
     # Expand sas calculated above by the probe sphere radius and fill the volume if there are any openings.
     grid = Grid.from_spheres(sas.surface_points, np.full(sas.surface_points.shape[0], args.probe_radius), grid_size=args.grid_size)
@@ -98,18 +99,8 @@ def main():
             print(verts)
 
         elif args.visualize == 'ply':
-            # import matplotlib.pyplot as plt
-            # from mpl_toolkits.mplot3d.art3d import Poly3DCollection
             print("Verts:", verts, "Faces:", faces, sep="\n")
-            # fig = plt.figure()
-            # ax = fig.add_subplot(111, projection='3d')
-            # mesh = Poly3DCollection(verts[faces])
-            # ax.add_collection3d(mesh)
-            # ax.set_xlim(0, grid.shape[0])
-            # ax.set_ylim(0, grid.shape[1])
-            # ax.set_zlim(0, grid.shape[2])
-            # plt.tight_layout()
-            # plt.show()
+            # visualization.matplotlib_mesh(verts, faces, grid.shape)
 
         elif args.visualize == 'xyz':
             print(verts)

protein_entrance_volume/boundary.py
@@ -6,17 +6,12 @@ import numpy as np
 from protein_entrance_volume import utils
 
 
-class Spheres:
-
-    def __init__(self, coords, radii, distance, centroid, extension=1):
+class Sphere:
+    def __init__(self, distance, centroid, extension=0, num_points=20000):
         # TODO: Need to determine the number of points by the distance
-        num_points = 1000
+        distance -= extension
         unit_sphere = utils.generate_sphere_points(num_points=num_points)
-        self._coords = np.append(coords, unit_sphere * distance + centroid, axis=0)
-
-        # Extend boundary spheres radii by extension and make a boundary fake
-        # sphere boundary with extension radius
-        self._radii = np.append(radii, np.full(num_points, extension))
+        self._coords = list(unit_sphere * distance + centroid)
 
     @property
     def coords(self):
@@ -25,18 +20,11 @@ class Spheres:
         """
         return self._coords
 
-    @property
-    def radii(self):
-        """
-        Convenience property to return an 1D array of atom radii
-        """
-        return self._radii
 
-
-class HalfSpheres:
-    def __init__(self, plane_coords, coords, radii, distance, centroid, opposing_point, extension=0):
+class HalfSphere:
+    def __init__(self, plane_coords, distance, centroid, opposing_point, extension=0, num_points=1000):
         # TODO: Need to determine the number of points by the distance
-        num_points = 1000
+        distance -= extension
         unit_sphere = utils.generate_sphere_points(num_points=num_points)
 
         plane = utils.best_fit_plane(plane_coords)
@@ -48,8 +36,7 @@ class HalfSpheres:
             if utils.side_point(plane, point) != side_point:
                 half_sphere_points.append(point)
 
-        self._coords = np.append(coords, half_sphere_points, axis=0)
-        self._radii = np.append(radii, np.full(num_points, extension))
+        self._coords = half_sphere_points
 
     @property
     def coords(self):
@@ -57,10 +44,3 @@ class HalfSpheres:
         Convenience property to return an 2D array of coordinate pairs
         """
         return self._coords
-
-    @property
-    def radii(self):
-        """
-        Convenience property to return an 1D array of atom radii
-        """
-        return self._radii

protein_entrance_volume/surface.py
@@ -15,7 +15,7 @@ class Surface:
     """
     _max_unit_distance = None
 
-    def __init__(self, coords, radii, centroid, num_points=100):
+    def __init__(self, coords, radii, centroid=None, boundary_points=None, num_points=100):
         self._coords = coords
         self._radii = radii
 
@@ -25,11 +25,17 @@ class Surface:
         # Build kdtree for spheres
         kdt = KDTree(self.coords, 10)
 
+        # Build kdtree for boundary_points
+        kdt_boundary = KDTree(boundary_points, 10)
+
         # The limit of how far to look for spheres in the kdtree
         twice_maxradii = self.radii.max() * 2
 
         # Create a set representing indices of s_on_i
         points_set = set(range(num_points))
+
+        # Possible points of the boundary
+        boundary_set = set(range(len(boundary_points)))
 
         # Start with None for the array
         surface_points = None
@@ -72,10 +78,19 @@ class Surface:
             # Get the surface points that are non-intersecting.
             available_coords = s_on_i[list(available_set)]
             if surface_points is None:
-                surface_points = available_coords
+                surface_points = list(available_coords)
             else:
-                surface_points = np.append(surface_points, available_coords, axis=0)
+                surface_points.extend(available_coords)
 
+            # Filter out any boundary points within the current sphere.
+            boundary_set -= set(kdt_boundary.query_ball_point(c_i, r_i))
+            # for i in range(boundary_points.shape[0]):
+
+
+        boundary_points = np.array(boundary_points)[list(boundary_set)]
+        surface_points.extend(list(boundary_points))
+        surface_points = np.array(surface_points)
+        # surface_points.extend(boundary_points)
         # FIXME: there is a chance we get the wrong surface so find a way to
         # verify if we have or not then loop through possible other starting
         # indexes.
@@ -104,6 +119,7 @@ class Surface:
 
         # The surface set should contain our distinct surface so index the
         # surface points using it.
+        surface_points = np.array(surface_points, dtype=np.float64)
         self._surface_points = surface_points[list(surface)]
 
     @property

protein_entrance_volume/utils.py
@@ -99,7 +99,8 @@ def generate_sphere_points(num_points=100):
     """
     indices = np.arange(0, num_points, dtype=float) + 0.5
     phi = np.arccos(1 - 2 * indices / num_points)
-    theta = np.pi * (1 + 5 ** 0.5) * indices
+    golden_ratio = (1 + 5 ** 0.5) / 2
+    theta = 2 * np.pi * indices / golden_ratio
 
     x = (np.cos(theta) * np.sin(phi))
     y = (np.sin(theta) * np.sin(phi))