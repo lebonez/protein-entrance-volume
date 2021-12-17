22d956658d70b828b96d9e32d4e6f00aecd151b9
lebonez <mitch_walls@hotmail.com>
2021-12-17 15:47:37 -0600

    linting

--- main.py
+++ main.py
@@ -69,7 +69,8 @@ def main():
     start = time_ns()
     args = parse_args()
     if len(args.outer_residues) < 3 or len(args.inner_residues) < 3:
-        msg = 'Number of integer values for "--outer-residues" and "--inner-residues" must be >= 3'
+        msg = 'Number of integer values for "--outer-residues" and ' \
+              '--inner-residues" must be >= 3'
         raise argparse.ArgumentError(None, msg)
 
     # Build the atoms dataframe object.
@@ -95,15 +96,19 @@ def main():
     mbr_radii = atoms_mbr[1] + args.probe_radius
 
     # Create a larger outer spherical boundary set of faux spheres with radius
-    # being the average distance from the centroid of all residues to the coords
-    # of all residues.
+    # being the average distance from the centroid of all residues to the
+    # coords of all residues.
     b_distance = utils.average_distance(atoms.arc, atoms.ar_coords)
-    # b_furthest_coord = atoms.or_coords[utils.furthest_node(atoms.arc, atoms.or_coords)]
+    # b_furthest_coord = atoms.or_coords[
+    #     utils.furthest_node(atoms.arc, atoms.or_coords)
+    # ]
     # b_distance = np.linalg.norm(b_furthest_coord - atoms.arc)
 
     # Calculate the optimal number of faux spheres so that the faux spheres are
     # within the probe_radius divided by the resolution to each other.
-    b_num_points = utils.sphere_num_points(b_distance, args.probe_radius / resolution)
+    b_num_points = utils.sphere_num_points(
+        b_distance, args.probe_radius / resolution
+    )
 
     # Generate the larger outer boundary sphere coords
     b_max = boundary.sphere(b_distance, atoms.arc, num_points=b_num_points)
@@ -117,38 +122,50 @@ def main():
 
     # No outer means to not create the outer residue faux spheres boundary
     if not args.no_outer:
-        o_num_points = utils.sphere_num_points(o_distance, args.probe_radius / resolution)
+        o_num_points = utils.sphere_num_points(
+            o_distance, args.probe_radius / resolution
+        )
         # Outer residue boundary half spheres coords.
-        b_outer = boundary.half_sphere(atoms.or_coords, o_distance, atoms.orc, atoms.irc, num_points=o_num_points)
+        b_outer = boundary.half_sphere(
+            atoms.or_coords, o_distance, atoms.orc, atoms.irc,
+            num_points=o_num_points
+        )
         # Append to previous copied array
         b_points = np.append(b_points, b_outer, axis=0)
     # No inner means to not create the inner residue faux spheres boundary
     if not args.no_inner:
-        i_num_points = utils.sphere_num_points(i_distance, args.probe_radius / resolution)
+        i_num_points = utils.sphere_num_points(
+            i_distance, args.probe_radius / resolution
+        )
         # Inner residue boundary half spheres coords.
-        b_inner = boundary.half_sphere(atoms.ir_coords, i_distance, atoms.irc, atoms.orc, num_points=i_num_points)
+        b_inner = boundary.half_sphere(
+            atoms.ir_coords, i_distance, atoms.irc, atoms.orc,
+            num_points=i_num_points
+        )
         # Append to previous copied array
         b_points = np.append(b_points, b_inner, axis=0)
 
-    # Build a radii array that has same length as boundary points and set the radii to the probe_radius
-    # This allows the SAS to directly translate to an SES along the spherical and half spherical boundaries coordinates.
+    # Build a radii array that has same length as boundary points and set the
+    # radii to the probe_radius. This allows the SAS to directly translate to
+    # an SES along the spherical and half spherical boundaries coordinates.
     b_radii = np.full(b_points.shape[0], args.probe_radius)
 
-    # Combine all of the boundary coords and radii with the mbr coords and radii.
+    # Combine all of the boundary coords and radii with the mbr coords and
+    # radii.
     coords = np.append(mbr_coords, b_points, axis=0)
     radii = np.append(mbr_radii, b_radii, axis=0)
 
     # Find a starting point for the surface search that is on the center of the
-    # surface of the outer residue half sphere..
-    # This is the most ideal location to start the search since it
-    # would be the actual beginning of the entrance.
+    # surface of the outer residue half sphere.This is the most ideal location
+    # to start the search since it would be the actual beginning of the
+    # entrance.
 
     # Best fit plane of the outer residue coords
     plane = utils.best_fit_plane(atoms.or_coords)
 
-    # Find our orientation of the normal by determining the side the inner residue
-    # centroid lies on and make sure the normal from the plane above points in the
-    # opposite direction.
+    # Find our orientation of the normal by determining the side the inner
+    # residue centroid lies on and make sure the normal from the plane above
+    # points in the opposite direction.
     opposing_side = utils.side_point(plane, atoms.irc)
 
     normal = plane[1]
@@ -164,7 +181,9 @@ def main():
     starting_point = (o_distance - args.probe_radius) * normal + atoms.orc
 
     # Generate the SAS grid from spherical points and radii within the MBR.
-    grid = Grid.from_cartesian_spheres(coords, radii, grid_size=args.grid_size, fill_inside=True)
+    grid = Grid.from_cartesian_spheres(
+        coords, radii, grid_size=args.grid_size, fill_inside=True
+    )
 
     # Find a good starting voxel to do SAS search by starting at the starting
     # point that was calculated above and moving towards the orc.
@@ -172,12 +191,12 @@ def main():
 
     i = 1
 
-    # Swap the normal back to point towards towards the orc from the starting point
+    # Swap the normal back to point towards towards the orc from the starting
+    # point.
     normal *= -1
 
     # Loop through and add ith iteration times the normal until we get an empty
     # (outside of spheres) voxel.
-    # test_points.append(starting_point)
     while grid.grid[starting_voxel[0], starting_voxel[1], starting_voxel[2]]:
 
         # Calculate the point using the ith magnitude normal
@@ -195,14 +214,20 @@ def main():
         # Increment the ith magnitude normal factor.
         i += 1
 
-    # Calculate the SAS volume nodes and SAS border nodes using connected_components
+    # Calculate the SAS volume nodes and SAS border nodes using
+    # connected components.
     nodes, sas_nodes = utils.connected_components(grid.grid, starting_voxel)
 
-    # A beginning first voxel for calculating the probe extended grid using the first sas node.
+    # A beginning first voxel for calculating the probe extended grid using
+    # the first sas node.
     voxel = np.array(np.unravel_index(sas_nodes[0], grid.shape))
 
-    # Build the initial grid using the first sas node voxel coordinate extended by the probe radius divided by grid size.
-    volume_grid = rasterize.sphere(voxel, args.probe_radius / args.grid_size, np.zeros(grid.shape, dtype=bool), fill_inside=False)
+    # Build the initial grid using the first sas node voxel coordinate
+    # extended by the probe radius divided by grid size.
+    volume_grid = rasterize.sphere(
+        voxel, args.probe_radius / args.grid_size,
+        np.zeros(grid.shape, dtype=bool), fill_inside=False
+    )
 
     # flatten array for the subsequent calculations.
     volume_grid = volume_grid.flatten()
@@ -210,19 +235,26 @@ def main():
     # Calculate the spherical 1D equation for the probe radius grid sphere
     eqn = np.argwhere(volume_grid).flatten() - sas_nodes[0]
 
-    # Apply all of the spheres at the remaining SAS border nodes using the sphere equation above calculated
-    # using the first sas node voxel.
+    # Apply all of the spheres at the remaining SAS border nodes using the
+    # sphere equation above calculated using the first sas node voxel.
     volume_grid = utils.eqn_grid(sas_nodes[1:], eqn, volume_grid)
 
-    # Set the SES nodes from the initial connected components run to true this fills any remaining holes
-    # which is faster then any binary fill method from other libraries.
+    # Set the SES nodes from the initial connected components run to true this
+    # fills any remaining holes which is faster then any binary fill method
+    # from other libraries.
     volume_grid[nodes] = True
 
-    # Build the SAS volume grid object which includes the center (non-border) voxels as well
-    # Note: it uses the previous grid zero shift attribute from the first atom coordinate based grid.
-    grid = Grid(volume_grid.reshape(grid.shape), zero_shift=grid.zero_shift, grid_size=args.grid_size)
+    # Build the SAS volume grid object which includes the center (non-border)
+    # voxels as well
+    # Note: it uses the previous grid zero shift attribute from the first atom
+    # coordinate based grid.
+    grid = Grid(
+        volume_grid.reshape(grid.shape), zero_shift=grid.zero_shift,
+        grid_size=args.grid_size
+    )
 
-    # Calculate the volume by counting true values in the above grid and multiplying by grid size cubed.
+    # Calculate the volume by counting true values in the above grid and
+    # multiplying by grid size cubed.
     volume_amount = np.count_nonzero(grid.grid) * (args.grid_size ** 3)
 
     # Print out the volume.
@@ -232,30 +264,47 @@ def main():
     print(f"Took: {end * 10 ** (-9)}s")
 
     if args.vertices_file:
-        # Run connected components on the volume grid to get SES border nodes for file generation
-        _, ses_nodes = utils.connected_components(np.invert(grid.grid), starting_voxel, border_only=True)
-        # Calculate center of voxels then scale and shift them back to the original atom coordinates system.
-        verts = (np.array(np.unravel_index(ses_nodes, grid.shape)).T + 0.5) * args.grid_size + grid.zero_shift
+        # Run connected components on the volume grid to get SES border nodes
+        # for file generation
+        _, ses_nodes = utils.connected_components(
+            np.invert(grid.grid), starting_voxel, border_only=True
+        )
+        # Calculate center of voxels then scale and shift them back to the
+        # original atom coordinates system.
+        verts = ((np.array(np.unravel_index(ses_nodes, grid.shape)).T + 0.5)
+            * args.grid_size + grid.zero_shift)
         if args.vertices_file.endswith(".xyz"):
-            # Convert SES nodes to coordinates in the original atom coordinate system.
-            # Generate an xyz file with atoms called X by far the slowest.
-            with open(args.vertices_file, 'w+', encoding='utf-8') as vertices_file:
-                vertices_file.write(f"{str(verts.shape[0])}\nVolume: {volume_amount} Å³\n")
+            # Convert SES nodes to coordinates in the original atom coordinate
+            # system. Generate an xyz file with atoms called X by far the
+            # slowest.
+            with open(args.vertices_file, 'w+', encoding='utf-8') \
+                    as vertices_file:
+                vertices_file.write(
+                    f"{str(verts.shape[0])}\nVolume: {volume_amount} Å³\n"
+                )
                 for vert in verts:
                     vert = list(vert)
                     vertices_file.write(f"X {vert[0]} {vert[1]} {vert[2]}\n")
         elif args.vertices_file.endswith(".csv"):
             # CSV file with one vertices x,y,z per row and x,y,z header
-            np.savetxt(args.vertices_file, verts, header="x,y,z", comments="", delimiter=",")
+            np.savetxt(
+                args.vertices_file, verts, header="x,y,z", comments="",
+                delimiter=","
+            )
         elif args.vertices_file.endswith(".txt"):
             # Dump vertices and volume to a space separated file
-            np.savetxt(args.vertices_file, verts, header=f"Volume: {volume_amount} Å³")
+            np.savetxt(
+                args.vertices_file, verts,
+                header=f"Volume: {volume_amount} Å³"
+            )
         elif args.vertices_file.endswith(".npz"):
             # Dump verts to a npz array file fastest and smallest way ideal
             # for doing post processing of the verts.
             np.savez_compressed(args.vertices_file, verts)
         else:
-            raise exception.InvalidFileExtension([".xyz", ".csv", ".txt", ".npz"])
+            raise exception.InvalidFileExtension(
+                [".xyz", ".csv", ".txt", ".npz"]
+            )
 
         # visualization.matplotlib_points(verts)
 
